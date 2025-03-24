93ba8fbd23b0022b5dc0aceae7e188fa101e6b94
Akram Rakhmetulla <akram042006@gmail.com>
2025-03-25 01:04:42 +0900

    Refactor Sparse Table

--- data_structures/sparse_table.cpp
+++ data_structures/sparse_table.cpp
@@ -2,13 +2,13 @@ template <typename T>
 struct sptable {
     vector<vector<T>> sp;
     veci lg;
-    int n, LOG;
+    int n, LOGN;
     
     sptable(const vector<T> &a) {
         n = a.size();
-        LOG = 0;
-        while((1 << LOG) <= n) ++LOG;
-        sp.resize(LOG, vector<T>(n + 1));
+        LOGN = 0;
+        while((1 << LOGN) <= n) ++LOGN;
+        sp.resize(LOGN, vector<T>(n + 1));
         lg.resize(n + 1);
         build(a);
     }
@@ -16,7 +16,7 @@ struct sptable {
     void build(const vector<T> &a) {
         for(int i = 1; i <= n; ++i) sp[i][0] = a[i];
         for(int i = 2; i < MAXN; ++i) lg[i] = lg[i >> 1] + 1;
-        for(int l = 1; l < LOG; ++l)
+        for(int l = 1; l < LOGN; ++l)
             for(int i = 1; i + (1 << l) - 1 <= n; ++i)
                 sp[l][i] = __gcd(sp[l - 1][i], sp[l - 1][i + (1 << (l - 1))]);
     }
