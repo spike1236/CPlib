56113b9e8cbde242902af26c59775b242a053d43
Akram Rakhmetulla <akram042006@gmail.com>
2025-03-27 21:58:02 +0900

    Removed redundant std

--- templates/multithreaded.cpp
+++ templates/multithreaded.cpp
@@ -11,17 +11,17 @@ class y_combinator_result {
     Fun fun_;
 public:
     template <class T>
-    explicit y_combinator_result(T&& fun) : fun_(std::forward<T>(fun)) {}
+    explicit y_combinator_result(T&& fun) : fun_(forward<T>(fun)) {}
     
     template <class... Args>
     decltype(auto) operator()(Args&&... args) {
-        return fun_(std::ref(*this), std::forward<Args>(args)...);
+        return fun_(ref(*this), forward<Args>(args)...);
     }
 };
 
 template <class Fun>
 decltype(auto) y_combinator(Fun&& fun) {
-    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+    return y_combinator_result<decay_t<Fun>>(forward<Fun>(fun));
 }
 
 using ll = long long;
